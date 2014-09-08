3235fa41ebbcf88f14e999a40fa7cfed8792aa81
Ezekiel <easyawos@ovi.com>
2014-09-08 19:35:04 +0100

    implementing DRY code using OOP

--- js/app.js
+++ js/app.js
@@ -1,45 +1,48 @@
+var ezeshopper= {
+	
+	onReady:function(){
+		$("#add_button").click(ezeshopper.add_item);
+		$('#item ul,#checked ul').on('change',"input[type=checkbox]", ezeshopper.checkoff);
+		$("div.list").on('click', 'a#del', ezeshopper.item_delete);
+	},
 
-
-
-
-/***add item to list***/
-
-$("#add_button").click(function(){
-	event.preventDefault();
-
-	if ($.trim($("#add_text").val()) !="") {
-
-	$('#item ul').append('<li>'+'<input type="checkbox">'+ $("#add_text").val()+ '<a href="#delete" id="del">delete</a>' + "</li>");
-
-	} 	else {
-			alert("PLease enter a valid item");
+	add_item: function(){
+		if (ezeshopper.validity()) {
+			$('#item ul').append('<li>'+'<input type="checkbox">'+ $("#add_text").val()+ '<a href="#" id="del">delete</a>' + "</li>");
+			$("#add_text").val("");
 		}
-	$("#add_text").val("");
-});
+	},
+
+	validity: function() {
+		if ($.trim($("#add_text").val()) !="") {
+			return $("#add_text");
+		}	else { 
+				alert("PLease enter a valid item");
+			}
+		$("#add_text").val("");
+	},
+
+	checkoff: function(){
+		var check_off = $(this).parent();
+		var direction = check_off.parents("#checked ul").length==0;
+
+		if(direction)
+		{
+			$('#checked ul').append(check_off);
+		}
+		else
+		{
+			$('#item ul').append(check_off);	
+		}
+	},
+
+	item_delete: function(){
+		var delete_item = $(this).parent();
+		delete_item.remove();
+		
+	}
 
 
-/***checkoff item***/
+};
 
-$('#item ul').on('change',"input[type=checkbox]", function(){
-	var check_off = $(this).parent();
-	$('#checked ul').append(check_off);
-
-});
-
-
-$('#checked ul').on('change',"input[type=checkbox]", function(){
-	var check_off = $(this).parent();
-	$('#item ul').append(check_off);
-
-});
-
-
-/***delete item***/
-
-$("#del").click(function(){
-	event.preventDefault();
-	alert("del");
-		$(this).parent().hide();
-
-});
-
+$(document).ready(ezeshopper.onReady);
