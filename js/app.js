36972a19384f48dab99210f00e26c79ad427e43d
Ezekiel <easyawos@ovi.com>
2014-09-08 16:25:34 +0100

    added functionality

--- js/app.js
+++ js/app.js
@@ -1,58 +1,45 @@
 
+
+
+
 /***add item to list***/
 
 $("#add_button").click(function(){
 	event.preventDefault();
 
 	if ($.trim($("#add_text").val()) !="") {
-		addcheckbox($("#add_text").val());
-		/*add_text.reset();*/
+
+	$('#item ul').append('<li>'+'<input type="checkbox">'+ $("#add_text").val()+ '<a href="#delete" id="del">delete</a>' + "</li>");
 
 	} 	else {
 			alert("PLease enter a valid item");
 		}
+	$("#add_text").val("");
 });
-
-
-function addcheckbox(shopping_item) {
-	console.log(shopping_item);
-	var container = $("#item");
-	var inputs = container.find("input");
-	var id = inputs.length+1;
-
-	$("<input />",{type: "label"} {type: "checkbox", id:id,  value: shopping_item}).appendTo(container);
-	$(container).append(shopping_item + '<br />');
-	
-}
-
 
 
 /***checkoff item***/
 
-$(document).on('change','[type=checkbox]',function(){ 
-	$(this).hide();
-	Check_off($(this).val());
+$('#item ul').on('change',"input[type=checkbox]", function(){
+	var check_off = $(this).parent();
+	$('#checked ul').append(check_off);
 
-	/*($(this).val()).hide();
-	var a = $(this).val();
-	a.hide();*/
 });
 
 
-function Check_off(checked_item) {
-	var container = $("#checked");
-	var inputs = container.find("input");
-	var id = inputs.length+1;
-	/*$("<input />", {type: "checkbox", id:id,  value: checked_item, checked: checked}).appendTo(container);*/
-	$(container).append(checked_item + '<br />');	
+$('#checked ul').on('change',"input[type=checkbox]", function(){
+	var check_off = $(this).parent();
+	$('#item ul').append(check_off);
 
-}
+});
 
 
+/***delete item***/
 
+$("#del").click(function(){
+	event.preventDefault();
+	alert("del");
+		$(this).parent().hide();
 
-
-
-
-
+});
 
